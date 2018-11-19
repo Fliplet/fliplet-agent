8436a29ce984d161d47a74ab2b3e1ba521699b8e
Nicholas Valbusa <squallstar@gmail.com>
2018-11-19 19:19:36 +0100

    tweaks

--- libs/logger.js
+++ libs/logger.js
@@ -1,6 +1,9 @@
 const chalk = require('chalk');
 const moment = require('moment');
+const fs = require('fs');
+const util = require('util');
 const Sentry = require('@sentry/node');
+const logFile = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 
 const isService = process.env.SERVICE;
 const logger = {};
@@ -10,13 +13,15 @@ function getDate() {
   return moment().format('YYYY-MM-DD HH:mm:ss');
 }
 
-  // Windows Service Logger to Event Viewer
+// Windows Service Logger to Event Viewer
 if (isService) {
   const EventLogger = require('node-windows').EventLogger;
-  eventLogger = new EventLogger('flipletagent.exe');
+  eventLogger = new EventLogger('Fliplet Agent');
 }
 
 logger.error = function (message) {
+  logFile.write(util.format(message) + '\n');
+
   if (isService) {
     return eventLogger.error(message);
   }
@@ -25,18 +30,22 @@ logger.error = function (message) {
 }
 
 logger.critical = function (message) {
+  logFile.write(util.format(message) + '\n');
+  Sentry.captureException(message);
+
   if (isService) {
     eventLogger.error(message);
   } else {
     console.error(chalk.red(`[${getDate()}]`), chalk.yellow('[ERROR]'), chalk.red(message));
+    console.error('A critical error was triggered. Aborting process.');
   }
 
-  Sentry.captureException(message);
-  console.error('A critical error was triggered. Aborting process.');
   process.exit(1);
 }
 
 logger.info = function (message) {
+  logFile.write(util.format(message) + '\n');
+
   if (isService) {
     return eventLogger.info(message);
   }
@@ -45,6 +54,8 @@ logger.info = function (message) {
 }
 
 logger.debug = function (message) {
+  logFile.write(util.format(message) + '\n');
+
   if (isService) {
     return eventLogger.info(message);
   }
