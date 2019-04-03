cbe83735afe50c3e0f1c7dcb2921f8ab7a160869
Nicholas Valbusa <squallstar@gmail.com>
2019-04-03 09:54:57 +0100

    catch all route

--- fliplet-agent.js
+++ fliplet-agent.js
@@ -9,4 +9,13 @@ program
   .command('start [path/to/config.js] [--test]', 'Start the agent in foregroud mode. Use the --test command to perform a dry run and avoid writing data to Fliplet servers.')
   .command('install [path/to/config.js]', 'Install a config file to run as a background service (Windows only).')
   .command('uninstall [path/to/config.js]', 'Uninstall a background service.')
+  .on('command:*', function (command) {
+    const firstCommand = command[0];
+
+    if (!this.commands.find(c => c._name == firstCommand)) {
+      console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
+      this.help();
+      process.exit(1);
+    }
+  })
   .parse(process.argv);
