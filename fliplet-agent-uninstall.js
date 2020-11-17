1c4ebf0f36f341e3e4d614b581fc561ac8e0fb4b
Nicholas Valbusa <squallstar@gmail.com>
2020-11-17 11:24:56 +0100

    ID-430 fixes for uninstalling the agent

--- fliplet-agent-uninstall.js
+++ fliplet-agent-uninstall.js
@@ -1,20 +1,73 @@
-const Service = require('node-windows').Service;
+const os = require('os');
+const path = require('path');
+
+let packageName;
+
+switch (os.platform()) {
+  case 'win32':
+    packageName = 'node-windows';
+    break;
+  case 'darwin':
+    packageName = 'node-mac';
+    break;
+  default:
+    throw new Error('OS not supported');
+}
+
+const Service = require(packageName).Service;
 
 const { Agent, config } = require('./libs/agent-bootstrap');
 
-// Create a new service object
-const svc = new Service({
-  name: 'Fliplet Agent',
-  script: require('path').join(__dirname, 'fliplet-agent-start.js')
-});
+try {
+  (new Agent(config.config)).then(function (agent) {
+    config.setup(agent, {
+      log
+    });
 
-svc.on('error', console.error);
+    if (!agent.operations.length) {
+      console.error('No operations have been defined.');
+    }
 
-// Listen for the "uninstall" event so we know when it's done.
-svc.on('uninstall', function (){
-  console.info('Uninstall complete.');
-  process.exit();
-});
+    const fileName = path.basename(config.path).split('.')[0];
+    const description = agent.operations[0].description;
 
-// Uninstall the service.
-svc.uninstall();
+    log.info(`Registering operation ${description} for file ${fileName}`);
+
+    // Create a new service object
+    const svc = new Service({
+      name: `Fliplet Agent (${fileName})`,
+      description,
+      script: require('path').join(__dirname, 'fliplet-agent-start.js'),
+      workingdirectory: require('path').join(__dirname, 'fliplet-agent-start.js'),
+      env: [
+        {
+          name: 'CFGPATH',
+          value: config.path
+        },
+        {
+          name: 'SERVICE',
+          value: true
+        }
+      ],
+      nodeOptions: [
+        '--harmony',
+        '--max_old_space_size=4096'
+      ]
+    });
+
+    svc.on('error', console.error);
+
+    svc.on('uninstall',function(){
+      console.info('The agent has been uninstalled successfully.');
+      process.exit();
+    });
+
+    // Uninstall the service.
+    svc.uninstall();
+
+  }).catch(function (err) {
+    log.critical(err);
+  });
+} catch (e) {
+  log.critical(`There was an error running your config file. (Error: ${e.message})`);
+}
